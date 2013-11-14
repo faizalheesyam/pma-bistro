ddb5bcdb3e061a4e45646bfc09072535ee275b55
Faizal Heesyam <faizalheesyam@gmail.com>
2013-11-14 10:55:09 +0800

    Initial files, based on pmahomme. Changed font to Ubuntu, darken sidebar, replaced PMA brand with MySQL logo.

---
+++ info.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Theme information
+ *
+ * @package    PhpMyAdmin-theme
+ * @subpackage PMAHomme
+ */
+
+/**
+ * If you have problems or questions about this theme email
+ * faizalheesyam@gmail.com
+ */
+
+/**
+ * Icon set used by this theme: http://famfamfam.com/lab/icons/silk/
+ */
+
+$theme_name = 'Bistro';
+$theme_full_version = '0.5';
+?>
